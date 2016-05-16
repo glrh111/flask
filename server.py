82432f0b06cf018b544cf34d5821a871ab2b66f9
glrh11 <glrh11@163.com>
2016-05-16 18:24:52 +0800

    add operations

--- server.py
+++ server.py
@@ -30,18 +30,25 @@ https://github.com/glrh111/
 
 from flask import *
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+app.config['SECRET_KEY'] = 'hah ni ma qu'
+
      
 # 定义一个公共的根目录
 
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return render_template('index.html')
-# 定义私人空间
+#    user_agent = request.headers.get('User-Agent')
+    return render_template('index.html', current_time=datetime.utcnow())
 
+
+# 定义私人空间
 
 @app.route('/usr/<name>')
 def usr(name):
@@ -49,7 +56,18 @@ def usr(name):
 
 @app.route('/usr/nb')
 def nb():
-	return redirect('http://localhost:5000/usr/wl')    
+	return redirect('http://localhost:5000/usr/wl')   
+
+# exception
+@app.errorhandler(400)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'), 500		 
+
+# enterence
 
 if __name__ == '__main__':
     app.run(debug=True)
