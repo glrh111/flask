8e442de53726caba27d5b3802ba0535722130c92
glrh11 <glrh11@163.com>
2016-05-17 14:20:54 +0800

    commit docs

--- server.py
+++ server.py
@@ -32,11 +32,40 @@ from flask import *
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+
+class NameForm(Form):
+	name = StringField("What's your name?", validators=[Required()])
+	submit = SubmitField('Submit')
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config['SECRET_KEY'] = 'hah ni ma qu'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://glrh11:qwe123@localhost/glrh11'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(64), unique = True)
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(64), unique = True, index = True)
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
 
      
 # 定义一个公共的根目录
@@ -44,19 +73,25 @@ app.config['SECRET_KEY'] = 'hah ni ma qu'
 
 @app.route('/')
 def index():
-#    user_agent = request.headers.get('User-Agent')
     return render_template('index.html', current_time=datetime.utcnow())
 
 
 # 定义私人空间
 
-@app.route('/usr/<name>')
-def usr(name):
-    return render_template('usr.html', name=name)
+@app.route('/usr', methods=['GET', 'POST'])
+def usr():
+	form = NameForm()
+	if form.validate_on_submit():
+		old_name = session.get('name')
+		if (old_name != None) and (old_name != form.name.data):
+			flash('name has changed')
+		session['name'] = form.name.data
+		return redirect(url_for('usr'))
+	return render_template('usr.html', form = form, name = session.get('name'))
 
 @app.route('/usr/nb')
 def nb():
-	return redirect('http://localhost:5000/usr/wl')   
+	return redirect('http://localhost:5000/usr')   
 
 # exception
 @app.errorhandler(400)
